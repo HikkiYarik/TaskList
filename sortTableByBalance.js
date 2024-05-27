db197b736b7e7a91d2d980aa06aeeda8d9b37418
Ярослав Демченко <wowkulaka555@gmail.com>
2024-05-27 08:19:03 +0800

    sort balance done

--- sortTableByBalance.js
+++ sortTableByBalance.js
@@ -1,6 +1,6 @@
 const sortBtnBalance = document.querySelector(".sort-balance-btn");
 const sortArrowEl = document.querySelector(".sort-balance-arrow");
-
+let switcher = false;
 sortBtnBalance.addEventListener("click", arrowBalanceHandler);
 sortBtnBalance.addEventListener("click", sortTableByBalance);
 function arrowBalanceHandler(event) {
@@ -17,13 +17,17 @@ function sortTableByBalance() {
   const table = document.getElementById("myTable");
   const tableRows = table.querySelectorAll("tr:not(thead>tr,tfoot>tr)");
   const arr = Array.from(tableRows, (row) => [row, +row.cells[3].textContent]);
-  arr.sort(([, a], [, b]) => a - b);
-  for (const [elem] of arr) {
-    table.insertAdjacentElement("beforeend", elem);
+  if (switcher === false) {
+    arr.sort(([, a], [, b]) => a - b);
+    for (const [elem] of arr) {
+      table.insertAdjacentElement("beforeend", elem);
+    }
+    switcher = true;
+  } else if (switcher === true) {
+    arr.sort(([, a], [, b]) => b - a);
+    for (const [elem] of arr) {
+      table.appendChild(elem);
+    }
+    switcher = false;
   }
-
-  // arr.sort(([, a], [, b]) => b - a);
-  // for (const [elem] of arr) {
-  //   table.appendChild(elem);
-  // }
 }
