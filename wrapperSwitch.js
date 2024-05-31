dd177284fb0434f683585bb06226a7a0fcd2cb63
Ярослав Демченко <wowkulaka555@gmail.com>
2024-05-31 14:14:59 +0800

    localStorage switch save`s

--- wrapperSwitch.js
+++ wrapperSwitch.js
@@ -11,6 +11,8 @@ function switchWrapper() {
     wrapper.classList.contains("active") &&
     wrapperUsers.classList.contains("d-none")
   ) {
+    localStorage.removeItem("switch");
+    localStorage.setItem("switch", "active");
     wrapper.classList.remove("active");
     wrapperUsers.classList.add("active");
     wrapper.classList.add("d-none");
@@ -26,6 +28,8 @@ function switchWrapper() {
     wrapperUsers.classList.contains("active") &&
     wrapper.classList.contains("d-none")
   ) {
+    localStorage.removeItem("switch");
+    localStorage.setItem("switch", "d-none");
     wrapperUsers.classList.remove("active");
     wrapper.classList.add("active");
     wrapperUsers.classList.add("d-none");
@@ -39,3 +43,33 @@ function switchWrapper() {
     headDiv.classList.add("bg-primary");
   }
 }
+
+let lastSelectedSwitch = localStorage.getItem("switch");
+function setSwitch(theme) {
+  if (theme === "active") {
+    wrapper.classList.remove("active");
+    wrapperUsers.classList.add("active");
+    wrapper.classList.add("d-none");
+    wrapperUsers.classList.remove("d-none");
+    switchBtn.classList.remove("btn-primary");
+    switchBtn.classList.add("btn-dark");
+    document.querySelector(".btn-switch").classList.remove("btn-primary");
+    document.querySelector(".btn-switch").classList.add("btn-dark");
+    switchText.textContent = "Users. Click for switch to 'Task list'";
+    headDiv.classList.remove("bg-primary");
+    headDiv.classList.add("bg-dark");
+  } else if (theme === "d-none") {
+    wrapperUsers.classList.remove("active");
+    wrapper.classList.add("active");
+    wrapperUsers.classList.add("d-none");
+    wrapper.classList.remove("d-none");
+    switchBtn.classList.remove("btn-dark");
+    switchBtn.classList.add("btn-primary");
+    document.querySelector(".btn-switch").classList.remove("btn-dark");
+    document.querySelector(".btn-switch").classList.add("btn-primary");
+    switchText.textContent = "Task list. Click for switch to 'Users'";
+    headDiv.classList.remove("bg-dark");
+    headDiv.classList.add("bg-primary");
+  }
+}
+setSwitch(lastSelectedSwitch);
